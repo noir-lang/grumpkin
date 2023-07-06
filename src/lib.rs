c28a245b2c21209784a1affe14a1206ad1cacc52
TomAFrench <tom@tomfren.ch>
2023-07-06 13:04:21 +0000

    chore: add feature flags to match arkworks

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(
+    warnings,
+    unused,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms
+)]
+#![forbid(unsafe_code)]
+
+#[cfg(feature = "curve")]
 pub mod curves;
+
 pub mod fields;
 
+#[cfg(feature = "curve")]
 pub use curves::*;
+
 pub use fields::*;
